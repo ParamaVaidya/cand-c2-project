4735fc9583e44120f9c69e3fc78b25b3b8ead009
ParamaVaidya <89422637+ParamaVaidya@users.noreply.github.com>
2021-08-23 17:02:20 -0500

    Delete Exercise_2 directory

--- Exercise_2/outputs.tf
+++
@@ -1,5 +0,0 @@
-# TODO: Define the output variable for the lambda function.
-output "lambda_output" {
-  description = "Lambda Function ID"
-  value = aws_lambda_function.UdacityLambda.id
-}
--- Exercise_2/variables.tf
+++
@@ -1,5 +0,0 @@
-# TODO: Define the variable for aws_region
-variable "aws_region" {
-  type = string
-  default = "us-east-1"
-}
